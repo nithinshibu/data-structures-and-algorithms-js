b3b69fd659c3d58e7dfb86c4e15d93c7ab5d546e
Nithin Mathew <nithin.mathew@sysfore.com>
2024-12-27 13:41:46 +0530

    O(n^2) complexity

--- Section01/Problems/quadratic_time.js
+++ Section01/Problems/quadratic_time.js
@@ -1,11 +1,28 @@
-//O(n^2)
+//Nested loop :- O(n^2)
 function findPairs(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       console.log(`Pair:${arr[i]} and ${arr[j]}`);
     }
   }
+  //O(n)
+  for (let q = 0; q < arr.length; q++) {
+    console.log("----", q);
+  }
+
+  //But the Big O here will be O(n^2)
 }
 
 const numbers = [1, 2, 3, 4, 5];
 findPairs(numbers);
+
+/*
+
+Here in this example , in the same function we have O(n2).and O(n) , so if we combine all the O operations , it becomes O(n2+n).
+
+Here  O(n2) is a dominant term and “n” is a non dominant term.
+
+So we remove the non-dominant term and we are only left with  O(n2) and this way we simplify our Big O Notation.
+
+
+*/
