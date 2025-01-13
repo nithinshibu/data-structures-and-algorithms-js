b429918401a9e38282150661ea92e47746c85710
Nithin Mathew <nithin.mathew@sysfore.com>
2025-01-13 14:28:46 +0530

    recursion changes

--- Section 07/Recursion/recursion.js
+++ Section 07/Recursion/recursion.js
@@ -8,12 +8,15 @@ function countDown(number) {
   countDown(number - 1);
 }
 
-function factorial(number) {
-  if (number === 0) {
-    return 1;
-  }
-  return number * factorial(number - 1);
-}
+// function factorial(number) {
+//   if (number === 0) {
+//     return 1;
+//   }
+//   return number * factorial(number - 1);
+// }
+
+const factorial = (number) =>
+  number === 0 ? 1 : number * factorial(number - 1);
 
 countDown(5);
 console.log(factorial(5));
