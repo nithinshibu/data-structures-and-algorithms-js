0886dd86fa2128b7e89072f0bc7a2b51c14e3ac1
Nithin Mathew <nithinshibumathew@gmail.com>
2025-01-13 15:01:25 +0530

    Breadth-first-search and Depth-first-search-pre-order in js

---
+++ Section 06/Tree/tree-traversal-bfs.js
@@ -0,0 +1,86 @@
+//Breadth First Search
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) {
+        return undefined;
+      }
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        } else {
+          temp = temp.left;
+        }
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        } else {
+          temp = temp.right;
+        }
+      }
+    }
+  }
+  includes(value) {
+    if (!this.root) {
+      return false;
+    }
+    let temp = this.root;
+    while (temp) {
+      if (value < temp.value) {
+        temp = temp.left;
+      } else if (value > temp.value) {
+        temp = temp.right;
+      } else if (value === temp.value) {
+        return true;
+      }
+    }
+    return false;
+  }
+  bfs() {
+    let current = this.root;
+    let queue = [];
+    let data = [];
+    queue.push(current);
+    while (queue.length) {
+      current = queue.shift();
+      data.push(current.value);
+      if (current.left) {
+        queue.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+      }
+    }
+    return data;
+  }
+}
+
+const tree = new BST();
+tree.insert(5);
+tree.insert(8);
+tree.insert(3);
+tree.insert(1);
+tree.insert(7);
+tree.insert(9);
+// console.log(tree.includes(31));
+// console.log(tree);
+console.log(tree.bfs()); //[ 5, 3, 8, 1, 7, 9 ]
